81a7d289cb3b4de502be48521982739c020ef14d
1279232215 <1279232215@qq.com>
2020-05-13 18:00:28 +0800

    修改1.0的Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -2,11 +2,7 @@ package com.usian.java;
 
 public class Test1 {
     public static void main(String[] args) {
-        //提交操作
-        System.out.println("111");
-        //更新操作
-        //解决冲突1
-        //解决冲突2
+        
         /*
         * 解决1.0bug
         * */
