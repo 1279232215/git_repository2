c6fd717c3055a9b7a27816bd3614a024abfb5d35
1279232215 <1279232215@qq.com>
2020-05-13 18:03:02 +0800

    修改1.0的Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -4,6 +4,10 @@ public class Test1 {
     public static void main(String[] args) {
         /*
         * 解决1.0bug
+        * 123
+        * 123
+        * 123
+        * 123
         * */
     }
 }
