c5ec281f3b2bd92f0e5023eeabbf11a7b1b16917
1279232215 <1279232215@qq.com>
2020-05-13 17:26:39 +0800

    提交操作

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -2,9 +2,13 @@ package com.usian.java;
 
 public class Test1 {
     public static void main(String[] args) {
+        //提交操作2
         System.out.println("111");
+        //更新操作2
+        //解决冲突12
+        //解决冲突22
         /*
-        * 解决1.0bug
+        * 解决1.0bug2
         * */
     }
 }
